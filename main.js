8691af78a8802c54223d69ba4a6b20f71e0dfb89
rohit chavhan <rohitchavhan46@gmail.com>
2024-05-05 20:09:52 +0530

    added all functionality keyboard shortcut removing

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-// round the number if it has number after decimal values
-// only one decimal is allowed
-// clear functionality
-// if user tries to divide by 0 return error
-// add a backspace button
-
-// first let calculator to handle simple stuff
-
 const currentMain = document.querySelector('.firstN');
-const previousMain = document.querySelector('.secondN');
 const decimalBtn = document.querySelector('.decimal');
 const clearBtn = document.querySelector('.clear');
 const numButtons = document.querySelectorAll('.num');
 const undoBtn = document.querySelector('.undo');
 const operatorBtn = document.querySelectorAll('.operator');
 const equalBtn = document.querySelector('.equal');
+const smallDisplay = document.querySelector('.thirdN');
 
 let previousNum = '';
 let currentNum = '';
 let mathOperators = '';
+let smallDis = '';
+let usedEqual = 0;
 
 function updateDispalys(){
-    previousMain.innerHTML = previousNum;
     currentMain.innerHTML = currentNum;
+    smallDisplay.innerHTML = smallDis;
 }
 
 numButtons.forEach(el => el.addEventListener('click', () => {
     currentNum += el.innerText;
+    smallDis += el.innerText;
     updateDispalys();
 }))
 
-// clear button
-clearBtn.addEventListener('click', () => {
+function clearFunc(){
     previousNum = '';
     currentNum = '';
     mathOperators = '';
+    smallDis = '';
     updateDispalys();
+}
+
+// clear button
+clearBtn.addEventListener('click', () => {
+    clearFunc();
 })
 
 // how to use decimal one times
@@ -44,6 +44,7 @@ decimalBtn.addEventListener('click', () => {
     }
     else {
         currentNum += decimalBtn.innerText;
+        smallDis += `${decimalBtn.innerText}`
         updateDispalys();
     }
 })
@@ -54,15 +55,10 @@ undoBtn.addEventListener('click', () => {
     updateDispalys();
 })
 
-// 3 things to see 
-// one when previous value is empty
-// second when previous value is filled 
-// and in both we have operator with us
-// and keep in mind of how would u display both screen
-
 function mathFunctions(mathSymbol){
     currentNum = Number(currentNum);
     previousNum = Number(previousNum);
+
     switch(mathSymbol){
         case '+':
             currentNum = currentNum + previousNum;
@@ -74,32 +70,67 @@ function mathFunctions(mathSymbol){
             currentNum = previousNum * currentNum;
             break;
         case '/':
+            console.log("great");
             currentNum = previousNum / currentNum;
             break;
     }
-    previousNum = '';
-    mathOperators = '';
-    updateDispalys();
 }
 
-function operations() {
-if(previousNum === '') {
-    previousNum = currentNum;
+function addintToSmall(x){
+    smallDis += ` ${x} `;
     currentNum = '';
-    updateDispalys();
 }
-else {
-    mathFunctions(mathOperators);
-}
+
+function operations(n) {
+    if(previousNum === '') {
+        previousNum = currentNum;
+        addintToSmall(n);
+        updateDispalys();
+    }
+    else if(usedEqual !== 0){
+        addintToSmall(n);
+        usedEqual -= 1;
+    }
+    else {
+        mathFunctions(mathOperators);
+        smallDis += ` = ${currentNum} ${n} `;
+        previousNum = currentNum;
+        currentNum = '';
+        mathOperators = '';
+        updateDispalys();
+    }
 }
 
 operatorBtn.forEach(el => el.addEventListener("click", () => {
-    console.log("great");
-    operations();
+    operations(el.innerText);
     mathOperators += el.innerText;
 }));
 
+function removeDecimal(n){
+    n = Number(n);
+    if(!Number.isInteger(n)) {
+        n = n.toFixed(3);
+        console.log(n);
+        currentNum = String(n);
+    }
+}
+
 equalBtn.addEventListener('click', () => {
-    console.log("equal");
+    console.log(mathOperators, currentNum);
+    if(mathOperators === '/' && currentNum === '0') {
+        clearFunc();
+        alert("u cannot divide number by zero which is an invalid operation in mathematics !");
+        return;
+    }
+    if(currentNum === '' || previousNum === ''){
+        clearFunc();
+        return;
+    }
+    usedEqual += 1;
     mathFunctions(mathOperators);
+    removeDecimal(currentNum);
+    smallDis += ` = ${currentNum} `;
+    previousNum = currentNum;
+    mathOperators = '';
+    updateDispalys();
 })
