805af06ac582056e545ed625d3ef8955fbd385bf
rohit chavhan <rohitchavhan46@gmail.com>
2024-05-06 13:27:25 +0530

    refactored the code little bit

--- main.js
+++ main.js
@@ -7,22 +7,30 @@ const clearBtn = document.querySelector('.clear');
 const operatorBtn = document.querySelectorAll('.operator');
 const equalBtn = document.querySelector('.equal');
 
+// storing data
+
 let previousNum = '';
 let currentNum = '';
 let mathOperators = '';
 let smallDis = '';
 let usedEqual = 0;
 
+// it updates main and previous displays
+
 function updateDispalys(){
     currentMain.innerHTML = currentNum;
     smallDisplay.innerHTML = smallDis;
 }
+
+// event listener to all buttons from 0 to 9
 
 numButtons.forEach(el => el.addEventListener('click', () => {
     currentNum += el.innerText;
     smallDis += el.innerText;
     updateDispalys();
 }));
+
+// wipe outs all data, start new
 
 function clearFunc(){
     previousNum = '';
@@ -33,14 +41,18 @@ function clearFunc(){
 }
 
 // clear button
+
 clearBtn.addEventListener('click', () => {
     clearFunc();
 });
+
+// decimal functionality
 
 function decimalFunc() {
     if(currentNum.includes('.')){
         return;
     }
+
     else {
         currentNum += decimalBtn.innerText;
         smallDis += `${decimalBtn.innerText}`
@@ -48,10 +60,13 @@ function decimalFunc() {
     }
 }
 
-// how to use decimal one times
+// decimal button
+
 decimalBtn.addEventListener('click', () => {
    decimalFunc();
 })
+
+// undo functionality
 
 function undoFunc(){
     currentNum = currentNum.slice(0, -1);
@@ -60,11 +75,15 @@ function undoFunc(){
 }
 
 // undo button
+
 undoBtn.addEventListener('click', () => {
     undoFunc();
 })
 
+// checks math operator and run that type of funcion on it
+
 function mathFunctions(mathSymbol){
+    console.log(`inside math function cNum is ${currentNum} pNum ${previousNum} and mathsymbol is ${mathSymbol}` );
     currentNum = Number(currentNum);
     previousNum = Number(previousNum);
 
@@ -79,7 +98,6 @@ function mathFunctions(mathSymbol){
             currentNum = previousNum * currentNum;
             break;
         case '/':
-            console.log("great");
             currentNum = previousNum / currentNum;
             break;
     }
@@ -90,17 +108,27 @@ function addintToSmall(x){
     currentNum = '';
 }
 
+/* grabbing number if smaller screen is empty when any operator is pressed  
+   we are just holding the current value in another varaible
+   else => if both values are filled we run our operations */
+
 function operations(n) {
+    console.log(n);
     if(previousNum === '') {
+        console.log("first if");
         previousNum = currentNum;
         addintToSmall(n);
         updateDispalys();
-    }
+    } 
+
     else if(usedEqual !== 0){
+        console.log("else if run of equal");
         addintToSmall(n);
         usedEqual -= 1;
     }
+
     else {
+        console.log("last else means math functions run");
         mathFunctions(mathOperators);
         smallDis += ` = ${currentNum} ${n} `;
         previousNum = currentNum;
@@ -115,27 +143,31 @@ operatorBtn.forEach(el => el.addEventListener("click", () => {
     mathOperators += el.innerText;
 }));
 
+// remove extra decimal we cap at 3
+
 function removeDecimal(n){
     n = Number(n);
     if(!Number.isInteger(n)) {
         n = n.toFixed(3);
-        console.log(n);
         currentNum = String(n);
     }
 }
-let kafka = 0;
+
+// equal button checks for running equal empty numbers
+
 equalBtn.addEventListener('click', () => {
-    kafka += 1;
-    console.log("kafka",kafka);
+    console.log("we pressed equal button");
     if(mathOperators === '/' && currentNum === '0') {
         clearFunc();
         alert("u cannot divide number by zero which is an invalid operation in mathematics !");
         return;
     }
+
     if(currentNum === '' || previousNum === ''){
         clearFunc();
         return;
     }
+
     usedEqual += 1;
     mathFunctions(mathOperators);
     removeDecimal(currentNum);
@@ -143,7 +175,9 @@ equalBtn.addEventListener('click', () => {
     previousNum = currentNum;
     mathOperators = '';
     updateDispalys();
-})
+});
+
+// handling keyboard input from user
 
 function handleKeyboardInput(event) {
     const pressedKey = event.key;
@@ -176,7 +210,6 @@ function handleKeyboardInput(event) {
     else if(pressedKey === 'c') {
         clearFunc();
     }
-    console.log(pressedKey,typeof pressedKey);
-  }
+};
 
-  document.addEventListener('keydown', handleKeyboardInput);
+document.addEventListener('keydown', handleKeyboardInput);
